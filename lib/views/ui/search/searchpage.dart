5775274447dcffd4b986a821250bce645d2be9d3
saadghazal <saad.m.ghazal@icloud.com>
2023-11-23 14:18:40 +0300

    search page created

--- lib/views/ui/search/searchpage.dart
+++ lib/views/ui/search/searchpage.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:jobhub/constants/app_constants.dart';
+import 'package:jobhub/views/common/exports.dart';
+import 'package:jobhub/views/common/height_spacer.dart';
+import 'package:jobhub/views/ui/search/widgets/custom_field.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -8,8 +14,51 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  TextEditingController searchController = TextEditingController();
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    searchController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(kOrange.value),
+        elevation: 0,
+        iconTheme: IconThemeData(color: Color(kLight.value)),
+        title: CustomField(
+          hintText: 'Search for a job',
+          controller: searchController,
+          suffixIcon: GestureDetector(
+            onTap: () {
+              setState(() {});
+            },
+            child: Icon(AntDesign.search1),
+          ),
+        ),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(20.h),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/optimized_search.png'),
+              HeightSpacer(size: 20),
+              ReusableText(
+                text: 'Start Searching for Jobs',
+                style: appstyle(
+                  24,
+                  Color(kDark.value),
+                  FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
