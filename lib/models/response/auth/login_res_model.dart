8eadf963c4547f118f74a576647eb6602b8cce4c
saadghazal <saad.m.ghazal@icloud.com>
2023-12-02 01:11:41 +0300

    update LoginResponseModel Structure

--- lib/models/response/auth/login_res_model.dart
+++ lib/models/response/auth/login_res_model.dart
@@ -1,3 +1,7 @@
+// To parse this JSON data, do
+//
+//     final loginResponseModel = loginResponseModelFromJson(jsonString);
+
 import 'dart:convert';
 
 LoginResponseModel loginResponseModelFromJson(String str) => LoginResponseModel.fromJson(json.decode(str));
@@ -5,21 +9,25 @@ LoginResponseModel loginResponseModelFromJson(String str) => LoginResponseModel.
 String loginResponseModelToJson(LoginResponseModel data) => json.encode(data.toJson());
 
 class LoginResponseModel {
+    String id;
+    String profileImage;
+    String userToken;
+
     LoginResponseModel({
         required this.id,
+        required this.profileImage,
         required this.userToken,
     });
 
-    final String id;
-    final String userToken;
-
     factory LoginResponseModel.fromJson(Map<String, dynamic> json) => LoginResponseModel(
         id: json["_id"],
+        profileImage: json["profile_image"],
         userToken: json["userToken"],
     );
 
     Map<String, dynamic> toJson() => {
         "_id": id,
+        "profile_image": profileImage,
         "userToken": userToken,
     };
 }
