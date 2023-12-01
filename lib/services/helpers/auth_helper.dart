64966e0d5979a6c0c7c892b02d70099c8b32b1c5
saadghazal <saad.m.ghazal@icloud.com>
2023-12-02 01:21:57 +0300

    complete login request function

--- lib/services/helpers/auth_helper.dart
+++ lib/services/helpers/auth_helper.dart
@@ -2,7 +2,9 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as https;
 import 'package:jobhub/models/request/auth/login_model.dart';
+import 'package:jobhub/models/response/auth/login_res_model.dart';
 import 'package:jobhub/services/config.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthHelper {
   static var client = https.Client();
@@ -16,6 +18,17 @@ class AuthHelper {
       body: jsonEncode(model),
     );
     if(response.statusCode == 200){
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+      String token = loginResponseModelFromJson(response.body).userToken;
+      String userId = loginResponseModelFromJson(response.body).id;
+      String profileImage = loginResponseModelFromJson(response.body).profileImage;
+
+
+      await prefs.setString('token', token);
+      await prefs.setString('user_id', userId);
+      await prefs.setString('profile_image', profileImage);
+      await prefs.setBool('logged_in', true);
 
       return true;
     }else{
