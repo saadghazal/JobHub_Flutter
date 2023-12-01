a105e524136456ecd806e44f0170d47d453b8bb3
saadghazal <saad.m.ghazal@icloud.com>
2023-12-02 01:46:54 +0300

    update loginNotifier entry_point and logged_in

--- lib/controllers/login_provider.dart
+++ lib/controllers/login_provider.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 
 class LoginNotifier extends ChangeNotifier {
+
   bool _obscureText = true;
 
   bool get obscureText => _obscureText;
@@ -9,5 +11,28 @@ class LoginNotifier extends ChangeNotifier {
     _obscureText = newState;
     notifyListeners();
   }
+  bool? _entryPoint;
 
+  bool get entryPoint => _entryPoint ?? false;
+  set entryPoint(bool newState){
+    _entryPoint = newState;
+    notifyListeners();
+  }
+
+  bool? _loggedIn;
+
+  bool get loggedIn => _loggedIn ?? false;
+  set loggedIn(bool newState){
+    _loggedIn = newState;
+    notifyListeners();
+  }
+
+  getPrefs()async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    entryPoint = prefs.getBool('entry_point') ?? false;
+    loggedIn = prefs.getBool('logged_in') ?? false;
+
+
+  }
 }
