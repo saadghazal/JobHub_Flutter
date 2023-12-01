f133b69b5158417593b79337ca9d6622b04f947b
saadghazal <saad.m.ghazal@icloud.com>
2023-12-02 01:05:58 +0300

    login request implemented

--- lib/services/helpers/auth_helper.dart
+++ lib/services/helpers/auth_helper.dart
@@ -1,6 +1,25 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as https;
+import 'package:jobhub/models/request/auth/login_model.dart';
+import 'package:jobhub/services/config.dart';
 
 class AuthHelper {
   static var client = https.Client();
 
+  static Future<bool> login({required LoginModel model}) async {
+    Map<String, String> requestHeaders = {'Content-Type': 'application/json'};
+    var url = Uri.https(Config.apiUrl, Config.loginUrl);
+    var response = await client.post(
+      url,
+      headers: requestHeaders,
+      body: jsonEncode(model),
+    );
+    if(response.statusCode == 200){
+
+      return true;
+    }else{
+      return false;
+    }
+  }
 }
