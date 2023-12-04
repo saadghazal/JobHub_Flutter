d4523ffe4b1b43a6f8d71a88f9643ddf58fe2659
saadghazal <saad.m.ghazal@icloud.com>
2023-12-04 20:08:19 +0300

    upload image to firebase

--- lib/controllers/image_provider.dart
+++ lib/controllers/image_provider.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:jobhub/constants/app_constants.dart';
+import 'package:uuid/uuid.dart';
 
 class ImageUploader extends ChangeNotifier {
+  var uuid = Uuid();
   final ImagePicker _picker = ImagePicker();
 
   String? imageUrl;
@@ -17,13 +22,14 @@ class ImageUploader extends ChangeNotifier {
     _imageFile = await cropImage(imageFile: _imageFile!);
     if (_imageFile != null) {
       imageFil.add(_imageFile.path);
+      await imageUpload(uploadedImage: _imageFile);
       imagePath = _imageFile.path;
     } else {
       return;
     }
   }
 
-  cropImage({required XFile imageFile}) async {
+  Future<XFile?> cropImage({required XFile imageFile}) async {
     CroppedFile? croppedFile = await ImageCropper.platform.cropImage(
       sourcePath: imageFile.path,
       compressQuality: 70,
@@ -45,5 +51,24 @@ class ImageUploader extends ChangeNotifier {
       return XFile(croppedFile.path);
     }
     return null;
+  }
+
+  Future<String?> imageUpload({required XFile uploadedImage}) async {
+    File image = File(uploadedImage.path);
+    try {
+      final ref = FirebaseStorage.instance
+          .ref()
+          .child('jobhub')
+          .child('${uuid.v1()}.jpg');
+
+      await ref.putFile(image);
+      imageUrl = await ref.getDownloadURL();
+
+      print(imageUrl);
+      return imageUrl;
+    } catch (e) {
+      print(e);
+      return null;
+    }
   }
 }
