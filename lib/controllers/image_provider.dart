94369fda415357173cbfee734001d297d03981b8
saadghazal <saad.m.ghazal@icloud.com>
2023-12-04 19:11:34 +0300

    implement image uploader provider

--- lib/controllers/image_provider.dart
+++ lib/controllers/image_provider.dart
@@ -1,3 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:jobhub/constants/app_constants.dart';
 
-class ImageUploader extends ChangeNotifier {}
+class ImageUploader extends ChangeNotifier {
+  final ImagePicker _picker = ImagePicker();
+
+  String? imageUrl;
+  String? imagePath;
+
+  List<String> imageFil = [];
+
+  void pickImage() async {
+    XFile? _imageFile = await _picker.pickImage(source: ImageSource.camera);
+
+    _imageFile = await cropImage(imageFile: _imageFile!);
+    if (_imageFile != null) {
+      imageFil.add(_imageFile.path);
+      imagePath = _imageFile.path;
+    } else {
+      return;
+    }
+  }
+
+  cropImage({required XFile imageFile}) async {
+    CroppedFile? croppedFile = await ImageCropper.platform.cropImage(
+      sourcePath: imageFile.path,
+      compressQuality: 70,
+      cropStyle: CropStyle.rectangle,
+      aspectRatioPresets: [CropAspectRatioPreset.ratio5x4],
+      uiSettings: [
+        AndroidUiSettings(
+          toolbarTitle: 'JobHub',
+          toolbarColor: Color(kLightBlue.value),
+          toolbarWidgetColor: Color(kLight.value),
+          initAspectRatio: CropAspectRatioPreset.ratio5x4,
+          lockAspectRatio: true,
+        ),
+        IOSUiSettings(title: 'JobHub'),
+      ],
+    );
+    if (croppedFile != null) {
+      notifyListeners();
+      return XFile(croppedFile.path);
+    }
+    return null;
+  }
+}
