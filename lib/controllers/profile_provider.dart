6486651b42a959c2bd0c18e8ed04c96a942fcc10
saadghazal <saad.m.ghazal@icloud.com>
2023-12-05 22:43:51 +0300

    getting user data in profile page

--- lib/controllers/profile_provider.dart
+++ lib/controllers/profile_provider.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-
+import 'package:jobhub/models/response/auth/profile_model.dart';
+import 'package:jobhub/services/helpers/auth_helper.dart';
 
 class ProfileNotifier extends ChangeNotifier {
- 
+  Future<ProfileRes>? profile;
+  getProfile() async {
+    profile = AuthHelper.getProfile();
+  }
 }
