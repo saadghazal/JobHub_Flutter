7919e6cde3f30c81a48f21ad3d1dcd0a2059ca80
saadghazal <saad.m.ghazal@icloud.com>
2023-12-12 16:43:18 +0300

    Get Jobs in JobsNotifier

--- lib/controllers/jobs_provider.dart
+++ lib/controllers/jobs_provider.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/foundation.dart';
-
+import 'package:jobhub/models/response/jobs/jobs_response.dart';
+import 'package:jobhub/services/helpers/jobs_helper.dart';
 
 class JobsNotifier extends ChangeNotifier {
- 
+  late Future<List<JobsResponse>> jobsList;
+  getJobs() {
+    jobsList = JobsHelper.getJobs();
+  }
 }
