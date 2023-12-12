2e66c6dc0c80c00362b7e5a87f2b4db97a140907
saadghazal <saad.m.ghazal@icloud.com>
2023-12-12 17:47:38 +0300

    Get Specific Job in the provider

--- lib/controllers/jobs_provider.dart
+++ lib/controllers/jobs_provider.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/foundation.dart';
+import 'package:jobhub/models/response/jobs/get_job.dart';
 import 'package:jobhub/models/response/jobs/jobs_response.dart';
 import 'package:jobhub/services/helpers/jobs_helper.dart';
 
 class JobsNotifier extends ChangeNotifier {
   late Future<List<JobsResponse>> jobsList;
   late Future<JobsResponse> recentJob;
+  late Future<GetJobRes> job;
   getJobs() {
     jobsList = JobsHelper.getJobs();
   }
 
   getRecentJob() {
     recentJob = JobsHelper.getRecentJob();
+  }
+
+  getJob({required String jobId}) {
+    job = JobsHelper.getJob(jobId: jobId);
   }
 }
